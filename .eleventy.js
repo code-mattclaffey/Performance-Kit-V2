0db9a63087abac579b4670bffed98dea4bb99ce6
matthew.claffey <matthew.claffey@onthebeach.co.uk>
2023-01-06 14:33:14 +0000

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,78 @@
+const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function(eleventyConfig) {
+  // Copy the `img` and `css` folders to the output
+  eleventyConfig.addPassthroughCopy("./_site/img");
+  eleventyConfig.addPassthroughCopy("./_site/css");
+
+  // Add plugins
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  });
+
+  // Customize Markdown library and settings:
+  let markdownLibrary = markdownIt({
+    html: true,
+    linkify: true
+  }).use(markdownItAnchor, {
+    permalink: markdownItAnchor.permalink.ariaHidden({
+      placement: "after",
+      class: "direct-link",
+      symbol: "#"
+    }),
+    level: [1,2,3,4],
+    slugify: eleventyConfig.getFilter("slugify")
+  });
+
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
+  return {
+    // Control which files Eleventy will process
+    // e.g.: *.md, *.njk, *.html, *.liquid
+    templateFormats: [
+      "md",
+      "njk",
+      "html",
+      "liquid"
+    ],
+
+    // Pre-process *.md files with: (default: `liquid`)
+    markdownTemplateEngine: "njk",
+
+    // Pre-process *.html files with: (default: `liquid`)
+    htmlTemplateEngine: "njk",
+
+    // -----------------------------------------------------------------
+    // If your site deploys to a subdirectory, change `pathPrefix`.
+    // Don’t worry about leading and trailing slashes, we normalize these.
+
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for link URLs (it does not affect your file structure)
+    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
+
+    // You can also pass this in on the command line using `--pathprefix`
+
+    // Optional (default is shown)
+    pathPrefix: "/",
+    // -----------------------------------------------------------------
+
+    // These are all optional (defaults are shown):
+    dir: {
+      input: "./_site",
+      includes: "_includes",
+      data: "_data",
+      output: "dist"
+    }
+  };
+};
---
+++ README.md
@@ -0,0 +1,29 @@
+# Performance Kit ⚡️
+
+[Production Website](https://performance-kit-v2.netlify.app/)
+
+## Getting Started
+
+### 1. Clone this Repository
+
+```
+git clone https://github.com/code-mattclaffey/performance-kit-v2 performance-kit
+```
+
+### 2. Navigate to the directory
+
+```
+cd performance-kit
+```
+
+### 3. Install dependencies
+
+```
+npm install
+```
+
+### 4. Run Eleventy
+
+```
+npm start
+```
